7f27ba0768c0ea6fc141096adddbfdc0cead77d6
Mohamedox <ghemd_m_dz@yahoo.fr>
2018-03-12 16:42:55 +0100

    share list completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,58 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser"
+import { ErrorHandler, NgModule } from "@angular/core"
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular"
+import { AngularFireModule } from "angularfire2"
+import { AngularFireDatabaseModule } from "angularfire2/database"
+import { environment } from "../environments/environment"
+import { SplashScreen } from "@ionic-native/splash-screen"
+import { StatusBar } from "@ionic-native/status-bar"
+import { AngularFireAuth } from "angularfire2/auth"
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component"
+import { HomePage } from "../pages/home/home"
+import { Item } from "../pages/item/item"
+import { ListPage } from "../pages/list/list"
+import { NewTodoPage } from "../pages/new-todo/new-todo"
+import { EditTodoPage } from "../pages/edit-todo/edit-todo"
+import { AuthentificationPage } from "../pages/authentification/authentification"
+import { ProfilPage } from "../pages/profil/profil"
+
+import { TodoServiceProvider } from "../services/todos.service"
 
 @NgModule({
   declarations: [
+    AuthentificationPage,
+    EditTodoPage,
+    Item,
+    ListPage,
     MyApp,
+    NewTodoPage,
+    ProfilPage,
     HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
+    AuthentificationPage,
+    EditTodoPage,
+    Item,
+    ListPage,
     MyApp,
+    NewTodoPage,
+    ProfilPage,
     HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    TodoServiceProvider,
+    AngularFireAuth
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,115 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core"
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  AlertController
+} from "ionic-angular"
+import { ListPage } from "../list/list"
+import { TodoList } from "../../models/TodoList"
+import { AngularFireAuth } from "angularfire2/auth"
 
+import { generateId } from "../../utils"
+import firebase from "firebase"
+
+import { TodoServiceProvider } from "../../services/todos.service"
+import { ProfilPage } from "../profil/profil"
+
+@IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  lists: TodoList[] = []
+  listsPending: boolean = true
+  ref: any
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public todoServiceProvider: TodoServiceProvider,
+    public alertCtrl: AlertController
+  ) {
+    this.afAuth.auth.onAuthStateChanged(user => {
+      if (!user) {
+        this.navCtrl.popToRoot()
+      }
+    })
 
+    this.todoServiceProvider.listenUser()
   }
 
+  ionViewWillEnter() {
+    this.listsPending = true
+    this.todoServiceProvider.getList().subscribe(lists => {
+      this.lists = lists
+      this.listsPending = false  
+    })
+  }
+
+  onListSelected(list: TodoList) {
+    this.navCtrl.push(ListPage, {
+      name: list.name,
+      listUuid: list.uuid
+    })
+  }
+
+  onAddList() {
+    let prompt = this.alertCtrl.create({
+      title: "Add list",
+      message: "Enter the name of the new list",
+      inputs: [
+        {
+          name: "title",
+          placeholder: "Title"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          handler: data => {}
+        },
+        {
+          text: "Add",
+          handler: data => {
+            this.todoServiceProvider.AddList(data.title)
+          }
+        }
+      ]
+    })
+    prompt.present()
+  }
+
+  onSeeProfile() {
+    const user = this.afAuth.auth.currentUser
+    if (user) {
+      this.navCtrl.push(ProfilPage, {
+        user: user
+      })
+    }
+  }
+
+  onDeleteList(uuid: string) {
+    let confirm = this.alertCtrl.create({
+      title: "Delete item",
+      message: "Are sure you want to delete this item?",
+      buttons: [
+        {
+          text: "Disagree",
+          handler: () => {
+            console.log("Disagree clicked")
+          }
+        },
+        {
+          text: "Agree",
+          handler: () => {
+            this.todoServiceProvider.deleteList(uuid)
+          }
+        }
+      ]
+    })
+    confirm.present()
+  }
 }
